a341856523d5f7a63c82b5dbd49efdfd23ec8e7e
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-03-01 18:28:24 +0000

    Redrew the horse at a more sensible size.

--- lib/wrath/creatures/horse.rb
+++ lib/wrath/creatures/horse.rb
@@ -8,11 +8,9 @@ class Horse < Mob
       vertical_jump: 0.1,
       horizontal_jump: 0.1,
       elasticity: 0.6,
-      factor_x: 1.4,
-      factor_y: 1.2,
       jump_delay: 2000,
       encumbrance: 0.4,
-      animation: "horse_8x8.png",
+      animation: "horse_12x11.png",
     }.merge! options
 
     super(options)
