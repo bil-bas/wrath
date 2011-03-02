5a2f58a7a36745c9208f23be872d4ed6c4e89e7a
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-03-02 01:30:32 +0000

    Improved paladin spawn.

--- lib/wrath/creatures/virgin.rb
+++ lib/wrath/creatures/virgin.rb
@@ -16,8 +16,8 @@ class Virgin < Mob
   end
 
   def ghost_disappeared
-    knight = Paladin.create(spawn: true)
-    $window.current_game_state.objects.push knight
-    $window.current_game_state.objects.push Horse.create(x: knight.x, y: knight.y - 4)
+    horse = Horse.create(spawn: true)
+    $window.current_game_state.objects.push horse
+    $window.current_game_state.objects.push Paladin.create(x: horse.x, y: horse.y - 6, y_velocity: 0.5, z_velocity: 0.5)
   end
 end
