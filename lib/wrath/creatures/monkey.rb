febcea0c040b35b7a9348ee6a41118c291057e3d
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-03-22 00:41:53 +0000

    Prevented monkey from jumping on someone who is being carried.

--- lib/wrath/creatures/monkey.rb
+++ lib/wrath/creatures/monkey.rb
@@ -19,7 +19,7 @@ module Wrath
     def on_collision(object)
       # Monkeys like to jump on your head.
       if object.is_a? Creature and object.controlled_by_player? and object.empty_handed? and
-          object.alive? and not @thrown_by.include? object and object != container
+          object.alive? and not @thrown_by.include? object and not object.inside_container?
 
         # Can jump from one to another guy.
         if inside_container?
