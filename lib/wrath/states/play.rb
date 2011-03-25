05853d9b66d1ec725b008df845be818ceb13f122
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-03-25 16:41:47 +0000

    Added Monk and Prophet as playable characters.

--- lib/wrath/states/play.rb
+++ lib/wrath/states/play.rb
@@ -23,7 +23,7 @@ class Play < GameState
       Message::StartGame
   ]
 
-  PRIEST_NAMES = %w[druidess priestess shaman witch]
+  PRIEST_NAMES = %w[druidess monk priestess prophet shaman witch]
   PRIEST_SPRITES = {}
   PRIEST_NAMES.each do |name|
     PRIEST_SPRITES[name] = "#{name}_8x8.png"
