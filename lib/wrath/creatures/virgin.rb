a86adab67c058f63e5eadb6da298c7019179000c
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-05-13 01:38:04 +0100

    Make sacrificing virgins not crash the game any more.

--- lib/wrath/creatures/virgin.rb
+++ lib/wrath/creatures/virgin.rb
@@ -11,13 +11,15 @@ class Virgin < Humanoid
     }.merge! options
 
     super options
+
+    @creator = parent # Save the creator, since #ghost_disappeared is called after we are destroyed.
   end
 
   def ghost_disappeared
-    unless parent.client?
-      horse = Horse.create(parent: parent)
-      parent.objects.push horse
-      paladin = Paladin.create(parent: parent, x: horse.x, y: horse.y - 6, y_velocity: 0.5, z_velocity: 0.5)
+    unless @creator.client?
+      horse = Horse.create(parent: @creator)
+      @creator.objects.push horse
+      paladin = Paladin.create(parent: @creator, x: horse.x, y: horse.y - 6, y_velocity: 0.5, z_velocity: 0.5)
       paladin.mount(horse)
     end
 
