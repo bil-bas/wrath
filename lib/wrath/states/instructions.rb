14eaedc506270cf4c0ca26c2f5cfef38f3d2fbf9
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-05-16 01:00:28 +0100

    Improved instructions by making 3 tabbed pages rather than trying to fit it all on one page.

--- lib/wrath/states/instructions.rb
+++ lib/wrath/states/instructions.rb
@@ -7,25 +7,30 @@ module Wrath
 
       on_input(:escape, :pop_game_state)
 
-      pack :vertical, padding: 20 do
-        label "Instructions for Wrath: Appease or Die!", font_size: 24
-        how_to_play = YAML.load(File.read(HOW_TO_PLAY))
+      pack :vertical, padding: 20, spacing: 10 do
+        label "Instructions", font_size: 24
 
-        pack :vertical, spacing: 0, padding: 0 do
-          how_to_play.each do |text|
-            case text[:type]
-              when :title
-                label text[:body]
-              when :block
-                text_area text: text[:body], font_size: 15, padding_h: 10, enabled: false, width: $window.width - 50
+        @how_to_play = YAML.load(File.read(HOW_TO_PLAY))
+
+        @tabs_group = group do
+          pack :horizontal, spacing: 10, padding: 0 do
+            @how_to_play.each_with_index do |page, i|
+              radio_button(page[:title], i)
             end
           end
+
+          subscribe :changed do |sender, value|
+            @body_text.text = @how_to_play[value][:body]
+          end
         end
+
+        @body_text = text_area font_size: 20, padding: 10, enabled: false,
+                               width: $window.width - 50, height: $window.height - 175
+
+        @tabs_group.value = 0
 
         button("Back") { pop_game_state }
       end
     end
-
-
   end
 end
