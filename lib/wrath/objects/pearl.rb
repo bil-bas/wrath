9cce0bc70f89d953ea32b5640741d4b0203084bf
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-06-02 00:19:25 +0100

    Forgot to make sacrificing the pearl of benefit :)

--- lib/wrath/objects/pearl.rb
+++ lib/wrath/objects/pearl.rb
@@ -4,8 +4,8 @@ module Wrath
       options = {
           animation: "pearl_7x7.png",
           factor: 0.75,
+          favor: 8,
           elasticity: 0.2,
-          contents: Pearl,
           encumbrance: 0.2,
           z_offset: -1,
       }.merge! options
