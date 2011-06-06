b4a2f50047335022c9b393505129b69a2a130a10
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-06-06 18:05:19 +0100

    Improved the "play" screen.

--- lib/wrath/states/play.rb
+++ lib/wrath/states/play.rb
@@ -14,16 +14,21 @@ module Wrath
       vertical do
         label "Play Wrath", font_size: 32
 
-        horizontal padding: 0 do
-          button("(O)ffline game", tip: 'Both players on the same keyboard') { local_game }
-          button("(J)oin Game", tip: 'Connect to a network game someone else is hosting') { join_game }
-          button("(H)ost Game", tip: 'Host a network game that that another player can join') { host_game }
+        vertical padding: 0, spacing: 10 do
+          width = 320
+          button("(O)ffline game", icon: icon('offline'), width: width, tip: 'Both players on the same keyboard') { local_game }
+          button("(J)oin Game", icon: icon('join'), width: width, tip: 'Connect to a network game someone else is hosting') { join_game }
+          button("(H)ost Game", icon: icon('host'), width: width, tip: 'Host a network game that that another player can join') { host_game }
         end
 
         horizontal padding: 0 do
           button("(B)ack") { pop_game_state }
         end
       end
+    end
+
+    def icon(type)
+      ScaledImage.new(Image["gui/play_#{type}.png"], $window.sprite_scale)
     end
 
     def local_game
