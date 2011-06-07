cce90344db84e2735fffb63f92797ad3765590c3
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-06-07 01:04:52 +0100

    Added pretty bullet points in instructions.

--- lib/wrath/states/instructions.rb
+++ lib/wrath/states/instructions.rb
@@ -12,6 +12,8 @@ module Wrath
 
         @how_to_play = YAML.load(File.read(HOW_TO_PLAY))
 
+        Gosu::register_entity(:bullet, Image['gui/bullet.png'])
+
         vertical spacing: 0, padding: 0 do
           @tabs_group = group do
             @tab_buttons = horizontal padding: 0, spacing: 5 do
@@ -21,13 +23,13 @@ module Wrath
             end
 
             subscribe :changed do |sender, value|
-              @body_text.text = @how_to_play[value][:body]
+              @body_text.text = @how_to_play[value][:body].gsub('*', '&bullet;')
               current = @tab_buttons.find {|elem| elem.value == value }
               @tab_buttons.each {|t| t.enabled = (t != current) }
             end
           end
 
-          @body_text = text_area font_size: 22, padding: 10, enabled: false,
+          @body_text = text_area font_size: 21, padding: 10, enabled: false,
                                  width: $window.width - 50, height: $window.height - 180
 
           @tabs_group.value = 0
