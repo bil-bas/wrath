8afd654e30bfd79d13e6a72c848f61984543ffc8
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-06-18 13:24:40 +0100

    Fixed lava.

--- lib/wrath/tiles/lava.rb
+++ lib/wrath/tiles/lava.rb
@@ -1,4 +1,7 @@
 module Wrath
+# Hot lava is hot.
+# If standing in it, you get very burned.
+# If standing on it, when it is  filled with rocks, it just sets you on fire.
 class Lava < Water
   include Fidgit::Event
 
@@ -6,7 +9,6 @@ class Lava < Water
   IMAGE_POSITION_FILLED = [2, 3]
 
   DAMAGE = 20 / 1000.0 # 20/second
-  FILLED_DAMAGE = 2 / 1000.0 # Still take some damage when standing on the rock.
   GLOW_COLOR = Color.rgba(255, 100, 0, 20)
   GLOW_SIZE = 0.65
 
@@ -23,9 +25,10 @@ class Lava < Water
         end
 
       when Creature
-        damage = filled? ? FILLED_DAMAGE : DAMAGE
-        object.wound(damage * parent.frame_time, self, :over_time) unless parent.client?
-        object.apply_status(:burning, duration: Status::Burning::BURN_DURATION)
+        unless parent.client?
+          object.wound(DAMAGE * parent.frame_time, self, :over_time) unless filled?
+          object.apply_status(:burning, duration: Status::Burning::DEFAULT_BURN_DURATION)
+        end
 
       else
         object.destroy
